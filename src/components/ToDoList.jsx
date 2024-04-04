aaa94d183dac3905da45ee6e79bdd5e7ab8be89b
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2024-04-04 12:24:18 +0200

    basic functionality done

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -14,12 +14,30 @@ export default function ToDoList() {
             setNewTask('');
         } 
     }
-    
-    function deleteTask(index) {}
 
-    function moveTaskUp(index) {}
+    function deleteTask(index) {
 
-    function moveTaskDown(index) {}
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+    }
+
+    function moveTaskUp(index) {
+        if (index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveTaskDown(index) {
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = 
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
 
     return (
         <div className="todo_list">
